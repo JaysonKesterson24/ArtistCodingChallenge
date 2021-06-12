e0377ce341efc42b491e0039108263559b812f2c
Jayson Kesterson <jaysonk@ravebizz.com>
2021-06-12 11:48:05 -0400

    Added View Binding to main activity and created instance of view model.

--- app/src/main/java/com/example/artistcodingchallenge/MainActivity.kt
+++ app/src/main/java/com/example/artistcodingchallenge/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.artistcodingchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.artistcodingchallenge.databinding.ActivityMainBinding
+import com.example.artistcodingchallenge.viewmodels.ArtistViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var viewModel : ArtistViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(ArtistViewModel::class.java)
+
     }
 }
