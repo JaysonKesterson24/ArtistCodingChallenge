a73be4898cc78fb426b2b09e05bc7b07d63c2ee2
Jayson Kesterson <jaysonk@ravebizz.com>
2021-06-12 15:54:13 -0400

    Added functionality for utilizing LiveData to update Recycler View and implemented Progress Bar.

--- app/src/main/java/com/example/artistcodingchallenge/MainActivity.kt
+++ app/src/main/java/com/example/artistcodingchallenge/MainActivity.kt
@@ -2,21 +2,57 @@ package com.example.artistcodingchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.artistcodingchallenge.adapters.ArtistRVAdapter
 import com.example.artistcodingchallenge.databinding.ActivityMainBinding
 import com.example.artistcodingchallenge.viewmodels.ArtistViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var viewModel : ArtistViewModel
+    private lateinit var trackAdapter : ArtistRVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setTitle("Track Searcher")
+
+        trackAdapter = ArtistRVAdapter()
 
         viewModel = ViewModelProvider(this).get(ArtistViewModel::class.java)
+
+        viewModel.getTracks().observe(this, Observer { trackList ->
+            binding.progressBar.visibility = View.INVISIBLE
+            trackAdapter.updateList(trackList)
+        })
+
+        with(binding) {
+            search_btn.setOnClickListener{
+                Log.d("_WORK", "Search Initiated")
+                if (artistSearchBox.text.toString().isNotEmpty()) {
+                    progressBar.visibility = View.VISIBLE
+                    viewModel.loadTracks(artistSearchBox.text.toString())
+                } else {
+                    Toast.makeText(applicationContext, "You Forgot to add a Search Term!", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            trackRv.apply {
+                layoutManager = LinearLayoutManager(
+                    context
+                )
+                adapter = trackAdapter
+            }
+        }
+
 
     }
 }
